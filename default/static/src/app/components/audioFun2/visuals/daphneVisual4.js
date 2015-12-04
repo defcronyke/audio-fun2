351112f929003470e704c99cdd94ae4139239f48
machinegirl <machinegirl@eternalvoid.net>
2015-12-03 20:18:19 -0500

    very good version of daphne visuals 4

--- default/static/src/app/components/audioFun2/visuals/daphneVisual4.js
+++ default/static/src/app/components/audioFun2/visuals/daphneVisual4.js
@@ -16,10 +16,22 @@ class DaphneVisual4 {
 		
 		c.fillStyle = 'rgba(0,0,0,1.0)';
 		c.fillRect(0,0,this.canvas[0].width,this.canvas[0].height);
-		let circles = [];
-		for (let i = 0; i < this.dataArray.length -1; i++) {
+		let x = -20;
+		let y = 20;
+		for (let i = 0; i < this.dataArray.length; i++) {
 			let data = this.dataArray[i];
-			
+			if ((x + 40) < this.canvas[0].width) {
+				x = x+40;
+			} else {
+				x = 20;
+				y = y+40;
+			}
+			let r = (1.0 - (Math.abs(128 - data)/128)) * 20;
+			let antiData = Math.floor((1.0 - (data/256)) * 255);
+			this.canvasContext.fillStyle = "rgba("+ data +","+ Math.abs(data - antiData)*2 +","+ antiData +",1.0)";
+			this.canvasContext.beginPath();
+			this.canvasContext.arc(x, y, r, 0, 2 * Math.PI, false);
+			this.canvasContext.fill();
 		}
 	}
 }
